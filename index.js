f2b0d8a233c0c4dece94c8fd7924cf29f9d434ad
Leandro Arguello <leanarguello012@gmail.com>
2020-08-28 16:04:45 -0300

    add gitignore

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 
-//Inicializo el client de discord
-const client = new Discord.Client();
+const client = new Discord.Client();//Inicializo el client de discord
+const prefix = "!";//Seteo un prefijo para que el bot reconozca los comandos
 
 //Funcionalidad
 client.on("message", function(message) {
-    if (message.author.bot) return;
+    if (message.author.bot) return; //Si el mensaje es de algun bot, no hagas nada.
+    if (!message.content.startsWith(prefix)) return; //Si el mensaje no comienza con el prefijo setteado: ignoralo.
   });
 
+  
 //Loggeo del bot
 client.login(config.BOT_TOKEN);
